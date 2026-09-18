b974d9adfb2a99d105a05265fb8022ef2115ebeb
NailongTwo <cld101010002@gmail.com>
2026-09-19 03:38:09 +0800

    Rename activity folders with leading zeros for sorting

--- Activity 10/DescendingPrinter.java
+++ Activity 10/DescendingPrinter.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public class DescendingPrinter{
 public static void main(String[] args){
-
 Scanner input = new Scanner(System.in);
 
-System.out.println("Enter a number:");
-int number = input.nextInt();
+    System.out.println("Enter a number:");
+    int number = input.nextInt();
 
-for (int i = number; i >= 1; i--){
-System.out.println(i);
-}
-System.out.println("Loop complete!");
-
+    for (int i = number; i >= 1; i--){
+    System.out.println(i);
+    }
+    System.out.println("Loop complete!");
+    input.close();
 }}
